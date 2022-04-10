136c57952c469792ed2872a50545dd3c2fa973ef
Jake <jake.ord345@gmail.com>
2022-04-10 19:32:49 +0100

    first commit

---
+++ src/lib/components/seo.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+  import { page } from "$app/stores";
+  export let title: string;
+  export let description: string =
+    "A Newcastle-Upon-Tyne based marketing designer currently looking for my next big role. I'm passionate about pushing the limits of brands through creative marketing strategies and striking design.";
+  export let url: string;
+</script>
+
+<svelte:head>
+  <title>{title} | Liam Johnston Creative Marketer</title>
+  <meta name="description" content={description} />
+  <meta
+    name="keywords"
+    content="Design, Marketing, Copywrighting, Graphic Design, Web, Mobile, Logo, Branding"
+  />
+  <link rel="canonical" href={`${$page.url.origin}${url}`} />
+</svelte:head>
---
+++ src/routes/index.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+  import { page } from "$app/stores";
+  import SEO from "$lib/components/seo.svelte";
+</script>
+
+<SEO title="Home" description="" url={$page.url.pathname} />
+<h1>Welcome to SvelteKit</h1>
