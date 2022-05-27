9c6d07a74a1302833ed2a2f263ebdb8fa68af715
Jake <jake.ord345@gmail.com>
2022-05-27 12:15:00 +0100

    changes

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -9,11 +9,6 @@
   export let projects: Project[];
 </script>
 
-<svelte:head>
-  <script
-    src="https://identity.netlify.com/v1/netlify-identity-widget.js"></script>
-</svelte:head>
-
 <Navbar {content} />
 
 <main class="container" id="content">
